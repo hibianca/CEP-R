14d26aa582efaa3d996fdc2214b147de40bc9aa9
hibianca <heybiancalima@gmail.com>
2023-08-11 11:59:16 -0300

    Project Files

---
+++ CEP+R/CEP_RApp.swift
@@ -0,0 +1,17 @@
+//
+//  CEP_RApp.swift
+//  CEP+R
+//
+//  Created by Bianca Nathally Bezerra de Lima on 25/05/23.
+//
+
+import SwiftUI
+
+@main
+struct CEP_RApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CEP+RUITests/CEP_RUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CEP_RUITestsLaunchTests.swift
+//  CEP+RUITests
+//
+//  Created by Bianca Nathally Bezerra de Lima on 25/05/23.
+//
+
+import XCTest
+
+final class CEP_RUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
